98d2275895673767d31a2fec8ce7a0ea05d49ea9
james@jamespamplin.com <james@jamespamplin.com>
2015-11-17 12:14:39 +0000

    Jade views with koa and nodemon reloading

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const koa = require( 'koa' ),
-  app = koa();
+  Jade = require( 'koa-jade' ),
 
-app.use( function *() {
-  this.body = 'Hello World';
+  app = koa(),
+  jade = new Jade( {
+    viewPath: './views',
+    debug: false,
+    pretty: true
+  } );
+
+
+app.use( jade.middleware );
+
+app.use( function* testMiddleware( next ) {
+  this.render( 'index' );
+  yield next;
 } );
 
 app.listen( 3000 );
